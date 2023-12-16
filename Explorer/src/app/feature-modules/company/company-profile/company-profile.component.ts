94825a4cc13ccd92d0574caa71a26ca753662c45
Nina <zarkokn@gmail.com>
2023-12-16 20:09:55 +0100

    Everything before Reservation

--- Explorer/src/app/feature-modules/company/company-profile/company-profile.component.ts
+++ Explorer/src/app/feature-modules/company/company-profile/company-profile.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
-import { Company } from '../model/companyModel';
+import { Company, CompanyEquipment } from '../model/companyModel';
 import { CompanyService } from '../company.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Equipment } from '../../equipment/model/equipmentModel';
+import { AvailableDate, Duration } from '../model/availableDateModel';
+import { DatePipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { AddAvailabledateFormComponent } from '../add-availabledate-form/add-availabledate-form.component';
 
 @Component({
   selector: 'xp-company-profile',
@@ -10,9 +15,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CompanyProfileComponent {
   company: Company;
+  selectedEquipment: CompanyEquipment[] = [];
+  filteredEquipments: CompanyEquipment[] = [];
+  searchValue: String;
+  isEquipmentChoosen: boolean = false;
+  isDone: boolean = false;
+  availableDates: AvailableDate[] = [];
+  shouldRenderAddDate: boolean = false;
+  shouldAdd0: boolean = false;
+  selectedDate: Date;
   
-  //shouldRenderUpdateForm: boolean = false;
-  constructor(private companyService: CompanyService,  private router: Router, private route: ActivatedRoute) { }
+  constructor(private companyService: CompanyService,  private router: Router, private route: ActivatedRoute, 
+    private datePipe: DatePipe, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
@@ -21,6 +35,17 @@ export class CompanyProfileComponent {
         this.companyService.getByName(companyName).subscribe({
           next: (c: Company) => {
             this.company = c;
+            this.filteredEquipments = c.equipmentSet;
+            
+            console.log("PRE");
+            console.log("ID: ", c.id);
+            this.companyService.getCompanyAvailableDates(this.company.id || 0).subscribe({
+              next: (dates: AvailableDate[]) => {
+                console.log("available dates: ",dates);
+                 this.availableDates = dates;
+              }
+            })
+
           },
           error: (err: any) => {
             console.log(err);
@@ -29,4 +54,92 @@ export class CompanyProfileComponent {
       }
     });
   }
+
+  selectEquipment(equipment: CompanyEquipment){
+    const index = this.selectedEquipment.indexOf(equipment);
+    if (index !== -1) {
+       this.selectedEquipment.splice(index, 1);
+    } else {
+      this.selectedEquipment.push(equipment);
+    }
+    console.log(this.selectedEquipment)
+  }
+
+  isSelected(equipment: any): boolean {
+    return this.selectedEquipment.includes(equipment);
+  }
+
+  doneChoosing(): void{
+    if (this.selectedEquipment.length != 0)
+      this.isDone = true;
+  }
+
+  onSearchChange(): void {
+    this.filterEquipments();
+  }
+  
+  private filterEquipments(): void {
+    this.filteredEquipments = this.company.equipmentSet.filter((e) =>
+      e.name.toLowerCase().match(this.searchValue.toLowerCase()) 
+    );
+  }
+
+  formatDateAndTime(localDateTime: string | object): { date: string, time: string } {
+    if (typeof localDateTime === 'object' && localDateTime !== null) {
+      localDateTime = localDateTime.toString(); 
+    }
+  
+    const dateTimeParts = localDateTime.split(',');
+  
+    const [year, month, day, hours, minutes] = dateTimeParts;
+    const dateString = `${year}-${month}-${day}`;
+    var timeString = '';
+    if(this.shouldAdd0 == false){
+      timeString = `${hours}:${minutes}`;
+    }
+    else{
+      timeString = `${hours}:${minutes}0`;
+    }
+  
+    return { date: dateString, time: timeString };
+  }
+  
+  addDate() : void {
+    const dialogRef = this.dialog.open(AddAvailabledateFormComponent, {
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Data:', result.selectedDate);
+        /*const formattedDate = result.selectedDate.toISOString().split('T')[0];
+        const formattedTime = result.selectedTime;
+        const dateTimeToSend = `${formattedDate} ${formattedTime}`;
+
+        const newAvailableDate: AvailableDate = {
+          admin: null,
+          startTime: dateTimeToSend,
+          duration: new Duration(),
+          adminConfirmationTime: new Date(),
+          confirmed: false,
+          selected: false
+        };
+
+        this.companyService.createAvailableDate(newAvailableDate);*/
+        result.selectedDate.setDate(result.selectedDate.getDate() + 1);
+        const dateString: string = result.selectedDate.toISOString();
+        this.companyService.getExtraAvailableDates(this.company.id || 0, dateString).subscribe({
+          next: (result: AvailableDate[]) => {
+              this.availableDates = result;
+              this.shouldAdd0 = true;
+          }
+        });
+      }
+    });
+  }
+
+  reserve() : void{
+
+  }
+  
 }
